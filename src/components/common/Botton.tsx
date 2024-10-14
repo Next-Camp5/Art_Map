58addd45a5bd537c0abb1ac76cb9e19aae98f81a
bomlang <bomlang4211@gmail.com>
2024-10-14 21:19:10 +0900

    initial setting

---
+++ src/components/common/Botton.tsx
@@ -0,0 +1,56 @@
+const createBgOrBorderMap = (
+  colors: typeof colorArray,
+  isBorder: boolean
+): BgOrBorderMap => {
+  return colors.reduce((acc, cur) => {
+    return {
+      ...acc,
+      [cur]: `${
+        isBorder
+          ? `border-${cur} border border-1 text-${cur} bg-white`
+          : `bg-${cur} text-white`
+      }`,
+    };
+  }, {} as BgOrBorderMap);
+};
+
+const colorArray = ["gray-3", "point", "primary"] as const;
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size: "XL" | "L" | "M" | "SM";
+  children?: React.ReactNode;
+  color: (typeof colorArray)[number];
+  border: boolean;
+}
+
+const SIZE_MAP: { [k in Props["size"]]: string } = {
+  XL: "w-[343px] h-[50px] rounded-[5px] text-md font-bold",
+  L: "w-[343px] h-[40px] rounded-[5px] text-sm font-bold",
+  M: "w-[165px] h-[40px] rounded-[5px] text-sm font-bold",
+  SM: "w-[88px] h-[30px] rounded-[15px] text-sm font-bold",
+};
+
+type BgOrBorderMap = {
+  [key in (typeof colorArray)[number]]: string;
+};
+
+const BG_COLOR_MAP = createBgOrBorderMap(colorArray, false);
+
+const BORDER_COLOR_MAP = createBgOrBorderMap(colorArray, true);
+
+const Button = ({ size, color, border, children, ...props }: Props) => {
+  const className = `${SIZE_MAP[size]} ${
+    border ? BORDER_COLOR_MAP[color] : BG_COLOR_MAP[color]
+  }`;
+
+  return (
+    <button
+      {...props}
+      className={`flex justify-center items-center ${className} font-bold hover: brightness-110`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,38 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#000000",
+        "primary-sub": "#757C86",
+        point: "#7F21F7",
+        "point-sub": "#E5D7F8",
+        warning: "#FF6060",
+        "warning-sub": "#FFE2E2",
+        success: "#7F21F7",
+        "success-sub": "#E5D7F8",
+        "gradient-point-1": "#5C61DE",
+        "gradient-point-2": "#AC7BFB",
+        "gray-0": "#FAFAFA",
+        "gray-1": "#F7F7F7",
+        "gray-2": "F0F0F0",
+        "gray-3": "#BEBEBE",
+        "gray-4": "#808080",
+        "gray-5": "444444",
+        "gray-6": "1A1A1A",
+      },
+
+      fontSize: {
+        md: "16px",
+        lg: "20px",
+        extra: "26px",
+        sm: "12px",
       },
     },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /^(bg|border)-(gray-3|point|primary)$/,
+    },
+  ],
 };
 export default config;
