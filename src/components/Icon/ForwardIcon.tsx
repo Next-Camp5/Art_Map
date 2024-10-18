c175ae4fc31103350c36852f2ac99e2fb5ac1094
pjc0179 <pjc0179@gmail.com>
2024-10-18 19:11:22 +0900

    feat:"forward아이콘 구현 및 back아이콘 router 뒤로가기 기능 제거 및 onClick을 props로 받게 변경"

---
+++ src/components/Icon/ForwardIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BaseIcon from "./BaseIcon";
+import { StrokeIconProps } from "./types/icon";
+
+const ForwardIcon = ({ stroke = "primary", ...props }: StrokeIconProps) => {
+  return (
+    <BaseIcon {...props}>
+      <path
+        d="M13.2251 5L20 11.775L13.2251 18.55"
+        className={`stroke-${stroke}`}
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.1445 11.772H3.94653"
+        className={`stroke-${stroke}`}
+        strokeWidth={2}
+        strokeLinecap="round"
+      />
+    </BaseIcon>
+  );
+};
+
+export default ForwardIcon;
