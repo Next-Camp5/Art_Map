2202a1e2965adcf7c39c317f89b6385b00f84065
pjc0179 <pjc0179@gmail.com>
2024-10-21 13:39:54 +0900

    feat:"배너 애니메이션 구현"

--- src/components/@Home/ExhibitionBanner.tsx
+++ src/components/@Home/ExhibitionBanner.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Scrollbar, EffectFade } from "swiper/modules";
 import "swiper/css";
@@ -9,9 +8,14 @@ import "swiper/css/scrollbar";
 import "swiper/css/effect-fade";
 
 import HomeHeader from "@/components/@Layout/HomeHeader";
-import Poster from "@/public/images/poster.png";
+import { useGSAP } from "@gsap/react";
+import { useState } from "react";
+import BannerItem from "./components/BannerItem";
 
 const ExhibitionBanner = () => {
+  const [active, setActive] = useState(0);
+
+  useGSAP(() => {}, []);
   return (
     <article className="bg-[#F15114] flex flex-col gap-[30px]">
       <HomeHeader />
@@ -24,35 +28,13 @@ const ExhibitionBanner = () => {
           fadeEffect={{ crossFade: true }}
           scrollbar={{ draggable: true }}
           modules={[Scrollbar, EffectFade]}
+          onSlideChange={(swiper) => setActive(swiper.activeIndex)}
           className="w-[300px] h-[430px]"
         >
           {[...Array(5)].map((_, index) => (
             <SwiperSlide key={index}>
               <Link href="/exhibition/detail" className="w-[300px] h-[430px]">
-                <div className="relative w-[300px] h-[430px] z-0">
-                  <Image
-                    src={Poster}
-                    alt={`포스터 ${index + 1}`}
-                    width={300}
-                    height={430}
-                  />
-                  <figcaption className="absolute bottom-0 left-0 p-4 z-20 flex flex-col gap-2">
-                    <p className="text-[26px] font-bold text-left text-white">
-                      Terrarium
-                    </p>
-                    <p className="text-[14px] font-bold text-left text-white">
-                      2021.08.17 ~ 2021.09.09
-                    </p>
-                    <p className="text-[14px] font-bold text-left text-white">
-                      중간지점
-                    </p>
-                  </figcaption>
-                  {/* Image 꾸밈 요소 */}
-                  <div
-                    aria-hidden="true"
-                    className="w-[300px] h-[200px] absolute bottom-0 bg-gradient-to-t from-[#1a1a1a]/60 to-[#1a1a1a]/0 z-10"
-                  ></div>
-                </div>
+                <BannerItem isActive={active === index} />
               </Link>
             </SwiperSlide>
           ))}
