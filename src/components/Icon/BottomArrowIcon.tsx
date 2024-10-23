ea4db7462b610946701d9990e0a82e1951ce4d16
pjc0179 <pjc0179@gmail.com>
2024-10-23 09:37:08 +0900

    feat:" X아이콘 BttonArrow아이콘 구현"

---
+++ src/components/Icon/BottomArrowIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import BaseIcon from "./BaseIcon";
+import { StrokeIconProps } from "./types/icon";
+import themeConfig from "@/configs/theme";
+
+const BottomArrowIcon = ({
+  stroke = themeConfig.DEFAULT_COLOR,
+  ...props
+}: StrokeIconProps) => {
+  return (
+    <BaseIcon {...props}>
+      <path
+        d="M18.4219 9.32812L11.9128 14.9192L5.17285 9.32812"
+        className={`stroke-${stroke}`}
+        stroke-width={2}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </BaseIcon>
+  );
+};
+
+export default BottomArrowIcon;
