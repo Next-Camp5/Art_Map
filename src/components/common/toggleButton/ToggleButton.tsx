84e7c0bf7579892ce018214d57c0c5b62e26563b
pjc0179 <pjc0179@gmail.com>
2024-10-23 09:37:28 +0900

    chore: "토글버튼 필요없는 state 제거"

--- src/components/common/toggleButton/ToggleButton.tsx
+++ src/components/common/toggleButton/ToggleButton.tsx
@@ -2,7 +2,7 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 interface Props {
   active?: boolean;
@@ -10,29 +10,27 @@ interface Props {
 }
 
 const ToggleButton = ({ active = false, onClick }: Props) => {
-  const [isActive, setIsActive] = useState<boolean>(active);
   const container = useRef<HTMLButtonElement>(null);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setIsActive(!isActive);
     onClick?.(e);
   };
 
   useGSAP(
     () => {
-      if (isActive) {
+      if (active) {
         gsap.to(".circle", { x: 20, duration: 0.3 });
       } else {
         gsap.to(".circle", { x: 0, duration: 0.3 });
       }
     },
-    { scope: container, dependencies: [isActive] }
+    { scope: container, dependencies: [active] }
   );
   return (
     <button
       ref={container}
       onClick={handleClick}
-      className={`h-[30px] w-[50px] rounded-[15px] p-[2px] ${isActive ? "bg-primary" : "bg-gray-3"}`}
+      className={`h-[30px] w-[50px] rounded-[15px] p-[2px] ${active ? "bg-primary" : "bg-gray-3"}`}
     >
       <div className=" circle bg-white h-[26px] w-[26px] rounded-full"></div>
     </button>
